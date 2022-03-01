046ff05304731c697cc5faf602f0d360f9b3a3da
Richard Ayotte <rich@ayottesoftware.com>
2022-03-01 11:55:32 -0500

    chore: update packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,11 @@ module.exports = {
 	}
 	, extends: [
 		'eslint:recommended'
-		, 'plugin:vue/essential'
+		, 'plugin:vue/vue3-strongly-recommended'
 		, 'plugin:@typescript-eslint/recommended'
 		, 'ayotte'
 	]
+	, parser: 'vue-eslint-parser'
 	, parserOptions: {
 		ecmaVersion: 'latest'
 		, parser: '@typescript-eslint/parser'
