1ea93bc9649f77e052eb4daa962af8c299034400
Richard Ayotte <rich@ayottesoftware.com>
2022-03-02 18:27:38 -0500

    feat: add logo and baking address

--- src/app.vue
+++ src/app.vue
@@ -1,3 +1,31 @@
 <template>
-  <h1>bākꜩ</h1>
+  <img
+    class="logo"
+    src="./assets/baktz-logo.svg"
+  >
+  <br>
+  <div class="delegate-address">
+    tz1R4PuhxUxBBZhfLJDx2nNjbr7WorAPX1oC
+  </div>
 </template>
+
+<style>
+body {
+  text-align: center;
+}
+
+.delegate-address {
+  background-color: #f3f3f3;
+  border: 1px solid silver;
+  display: inline-block;
+  font-family: monospace;
+  font-size: 20pt;
+  padding: 15px 25px;
+  text-align: center;
+}
+
+.logo {
+  display: inline-block;
+  padding: 15mm;
+}
+</style>
