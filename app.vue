36f7a860bed49ffee926ab649b01943f92e49b00
Richard Ayotte <rich@ayottesoftware.com>
2022-08-06 13:47:22 -0400

    feat: copy delegation address to clipboard

--- app.vue
+++ app.vue
@@ -1,38 +1,59 @@
 <template>
-  <img
-    class="logo"
-    height="184"
-    src="~assets/images/baktz-logo.svg"
-    width="529"
-  >
-  <h1>
-    Tezos baking service.
-  </h1>
-  <code class="delegate-address">tz1R4PuhxUxBBZhfLJDx2nNjbr7WorAPX1oC</code>
+  <div class="container">
+    <img
+      class="logo"
+      height="184"
+      src="~assets/images/baktz-logo.svg"
+      width="529"
+    >
+    <div class="delegate-address-container">
+      <div class="delegate-address">
+        {{ delegateAddress }}
+      </div>
+      <button
+        @click="copyToClipboard(delegateAddress)"
+        class="p-button-secondary"
+      >
+        Copy to clipboard
+      </button>
+    </div>
+  </div>
 </template>
 
-<style>
-body {
-  font-family: 'Alegreya Sans', sans-serif;
+<script setup lang="ts">
+const delegateAddress = `tz1R4PuhxUxBBZhfLJDx2nNjbr7WorAPX1oC`
+
+const copyToClipboard = async function(text: string) {
+	await navigator.clipboard.writeText(text)
+}
+
+</script>
+
+<style lang="scss" scoped>
+.container {
+  width: 100%;
   text-align: center;
+  margin: 0 auto;
 }
 
-h2 {
-  margin-top: 0px;
-  color: #444;
-}
-
-.delegate-address {
+.delegate-address-container {
   background-color: #f3f3f3;
   border: 1px solid silver;
-  display: inline-block;
-  font-family: 'JetBrains Mono', monospace;
-  font-size: x-large;
-  margin: 30px;
-  max-width: 80%;
+  display: block;
+  margin: 10px auto;
+  max-width: 15ex;
   padding: 15px;
-  text-align: center;
-  word-wrap: break-word;
+  aspect-ratio: 1 / 1;
+  .delegate-address {
+    font-family: monospace;
+    font-size: x-large;
+    margin: 10px;
+    text-align: justify;
+    word-wrap: break-word;
+  }
+  .p-button-secondary {
+    width: 100%;
+  }
 }
 
 .logo {
