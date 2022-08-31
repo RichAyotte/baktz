c75bf009eb0063584eec8d258b0933cb96137bf7
Richard Ayotte <rich@ayottesoftware.com>
2022-08-30 21:23:41 -0400

    chore: prettier config

---
+++ prettier.config.cjs
@@ -0,0 +1,17 @@
+/* eslint-env node */
+module.exports = {
+	printWidth: 80,
+	tabWidth: 1,
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+	quoteProps: 'as-needed',
+	jsxSingleQuote: true,
+	bracketSpacing: true,
+	bracketSameLine: false,
+	arrowParens: 'avoid',
+	vueIndentScriptAndStyle: false,
+	endOfLine: 'lf',
+	embeddedLanguageFormatting: 'auto',
+	singleAttributePerLine: true,
+}
