ffa1e059b3d0e73b28da44300333df5c85b9090c
Richard Ayotte <rich@ayottesoftware.com>
2022-10-05 13:29:23 -0400

    feat: put features in carousel for mobile

--- components/section-features.vue
+++ components/section-features.vue
@@ -1,24 +1,49 @@
 <template>
 	<section class="section-features">
 		<h2>Features</h2>
-		<feature-box
-			class="feature-box"
-			v-for="(feature, index) in features"
-			:key="index"
-			:title="feature.title"
-			:description="feature.description"
-			:icon-url="feature.iconUrl"
-		/>
+		<div id="feature-boxes-container">
+			<!-- 			<feature-box
+				v-for="(feature, index) in features"
+				:key="index"
+				class="feature-box"
+				:title="feature.title"
+				:description="feature.description"
+				:icon-url="feature.iconUrl"
+			/>
+ -->
+			<carousel
+				class=""
+				:items-to-show="1"
+			>
+				<slide
+					v-for="(feature, index) in features"
+					:key="index"
+					><feature-box
+						class="feature-box"
+						:title="feature.title"
+						:description="feature.description"
+						:icon-url="feature.iconUrl"
+					/>
+				</slide>
+				<template #addons>
+					<navigation />
+					<pagination />
+				</template>
+			</carousel>
+		</div>
 	</section>
 </template>
 
 <script setup lang="ts">
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+
 import bellConciergeSolid from '~/assets/images/bell-concierge-solid.svg'
-// import buildingColumns from '~/assets/images/building-columns-solid.svg'
-// import cashCoinsImg from '~/assets/images/cash-coins.svg'
+import buildingColumns from '~/assets/images/building-columns-solid.svg'
+import cashCoinsImg from '~/assets/images/cash-coins.svg'
 import envelopeSolid from '~/assets/images/envelope-solid.svg'
 import moneyBillImg from '~/assets/images/money-bill-1-wave-solid.svg'
-// import peopleGroupSolid from '~/assets/images/people-group-solid.svg'
+import peopleGroupSolid from '~/assets/images/people-group-solid.svg'
 
 const features = [
 	{
@@ -37,28 +62,73 @@ const features = [
 		description: `Payouts to delegates are automated at the end of each cycle`,
 		iconUrl: envelopeSolid,
 	},
-	// {
-	// 	title: `10 XTZ Minimum`,
-	// 	description: `Delegators must delegate at least 10 XTZ to receive a payout`,
-	// 	iconUrl: cashCoinsImg,
-	// },
-	// {
-	// 	title: `Active Voter`,
-	// 	description: `We actively seek community feedback and vote on all phases of the voting process`,
-	// 	iconUrl: buildingColumns,
-	// },
-	// {
-	// 	title: `Community Engaged`,
-	// 	description: `Active Tezonian since 2017 focused on driving Tezos forward`,
-	// 	iconUrl: peopleGroupSolid,
-	// },
+	{
+		title: `10 XTZ Minimum`,
+		description: `Delegators must delegate at least 10 XTZ to receive a payout`,
+		iconUrl: cashCoinsImg,
+	},
+	{
+		title: `Active Voter`,
+		description: `We actively seek community feedback and vote on all phases of the voting process`,
+		iconUrl: buildingColumns,
+	},
+	{
+		title: `Community Engaged`,
+		description: `Active Tezonian since 2017 focused on driving Tezos forward`,
+		iconUrl: peopleGroupSolid,
+	},
 ]
 </script>
 
+<style lang="scss">
+@import 'include-media';
+.carousel__slide {
+	box-sizing: border-box;
+	padding: 0 20px;
+}
+
+.carousel__pagination-button {
+	padding: 10px;
+}
+.carousel__next,
+.carousel__prev {
+	top: 40%;
+	opacity: 0.5;
+}
+.carousel__next {
+	right: -15px;
+}
+.carousel__prev {
+	left: -15px;
+	opacity: 0.5;
+}
+
+
+@include media('>tablet') {
+	.carousel__next,
+	.carousel__prev,
+	.carousel__pagination {
+		display: none;
+	}
+
+	.carousel__track {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 20px;
+	}
+	.carousel__slide {
+		flex: 1 1 20vw;
+		min-width: 30%;
+	}
+}
+
+</style>
+
 <style lang="scss" scoped>
 @import 'assets/styles/variables';
+@import 'include-media';
 
-// .section-features {
+// #feature-boxes-container {
 // 	background-image: url(/assets/images/wave-line.svg);
 // 	background-position: center;
 // 	background-repeat: no-repeat;
@@ -93,7 +163,7 @@ const features = [
 // 	}
 // }
 
-.section-features {
+#feature-boxes-container {
 	background-image: url(/assets/images/wave-line.svg);
 	background-position: center;
 	background-repeat: no-repeat;
@@ -118,8 +188,7 @@ const features = [
 	border-color: #665c82;
 	border-style: solid;
 	border-width: 1px;
-	flex: 1 1 20vw;
 	padding: 2rem;
-	// min-width: 70%;
+	height: 100%;
 }
 </style>
