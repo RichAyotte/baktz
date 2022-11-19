89d12bfa7220905792a97d45ca3b824b438ee7f2
Richard Ayotte <rich@ayottesoftware.com>
2022-11-19 16:06:22 -0500

    fix: update nuxt.config for v3, add title and cleanup tsconfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,32 +1,34 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt/config'
 import pkg from './package.json'
 
 export default defineNuxtConfig({
 	css: ['@/assets/styles/global.scss'],
-	meta: {
-		title: pkg.name,
-		meta: [
-			{
-				hid: 'description',
-				name: 'description',
-				content: pkg.description,
-			},
-		],
-		link: [
-			{
-				href: `https://fonts.googleapis.com`,
-				rel: `preconnect`,
-			},
-			{
-				href: `https://fonts.gstatic.com`,
-				rel: `preconnect`,
-				crossorigin: true,
-			},
-			{
-				href: `https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@600&family=Roboto:wght@300&display=swap`,
-				rel: `stylesheet`,
-			},
-		],
+	app: {
+		head: {
+			title: pkg.name,
+			meta: [
+				{
+					hid: 'description',
+					name: 'description',
+					content: pkg.description,
+				},
+			],
+			link: [
+				{
+					href: `https://fonts.googleapis.com`,
+					rel: `preconnect`,
+				},
+				{
+					href: `https://fonts.gstatic.com`,
+					rel: `preconnect`,
+					crossorigin: 'anonymous',
+				},
+				{
+					href: `https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@600&family=Roboto:wght@300&display=swap`,
+					rel: `stylesheet`,
+				},
+			],
+		},
 	},
 	ssr: false,
 })
