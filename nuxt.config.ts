1e16dbdb01ed68c497b578574f98ddfec5b9e8c3
Richard Ayotte <rich@ayottesoftware.com>
2023-05-13 18:03:44 -0400

    feat: add pwa icons and settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,6 +26,25 @@ export default defineNuxtConfig({
 					as: `style`,
 					onload: `this.rel='stylesheet'`,
 				},
+				{
+					rel: 'apple-touch-icon',
+					sizes: '180x180',
+					href: '/apple-touch-icon.png',
+				},
+				{
+					rel: 'icon',
+					type: 'image/png',
+					sizes: '32x32',
+					href: '/favicon-32x32.png',
+				},
+				{
+					rel: 'icon',
+					type: 'image/png',
+					sizes: '16x16',
+					href: '/favicon-16x16.png',
+				},
+				{ rel: 'manifest', href: '/site.webmanifest' },
+				{ rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
 			],
 			meta: [
 				{
@@ -33,6 +52,8 @@ export default defineNuxtConfig({
 					name: 'description',
 					content: pkg.description,
 				},
+				{ name: 'msapplication-TileColor', content: '#da532c' },
+				{ name: 'theme-color', content: '#ffffff' },
 			],
 			title: pkg.name,
 		},
