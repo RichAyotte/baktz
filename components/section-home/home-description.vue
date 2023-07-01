cf1c4834748f69ddf62e54980bd337103f5899e8
Richard Ayotte <rich@ayottesoftware.com>
2023-07-01 19:20:15 -0400

    feat: implement delegate / disconnect

--- components/section-home/home-description.vue
+++ components/section-home/home-description.vue
@@ -10,41 +10,53 @@
 			/>
 			is a secure, reliable, and community involved Tezos baking service.
 		</p>
-		<p>Copy the address below and paste it into your wallet's baking feature.</p>
-		<div
-			id="delegate-address-container"
-			@click="copyToClipboard(baktzDelegateAddress)"
-		>
-			<div id="delegate-address">
-				{{ baktzDelegateAddress }}
-			</div>
-			<nuxt-img
-				alt="Clipboard"
-				src="clipboard.svg"
-				style="aspect-ratio: 1/1"
-			/>
-		</div>
-		<br />
-		<div id="button-or-address">
+		<client-only>
 			<span v-if="$activeAccount != null"
-				><strong>{{ $activeAccount.address.substring(0, 8) }}&#8230;</strong> is
-				currently delegating to
+				><strong
+					><a
+						target="_blank"
+						:href="`https://tzkt.io/${$activeAccount.address}/operations/`"
+						>{{ $activeAccount.address }}</a
+					></strong
+				>
+				is currently delegated to
 				<nuxt-img
 					alt="Baktz logo"
 					class="img-text"
 					src="baktz-logo.svg"
 					style="aspect-ratio: 361/127"
 				/>
-				Thank you!<button @click="switchWallet">Switch wallet</button></span
+				<br /><br />
+				<button @click="$disconnect">Disconnect</button></span
 			>
-			<button
-				v-else
-				id="delegate-button"
-				@click="$delegate"
-			>
-				Delegate now
-			</button>
-		</div>
+			<span v-else>
+				<button
+					id="delegate-button"
+					@click="$delegate"
+				>
+					Delegate to Baktz
+				</button>
+				<br /><br />
+				<strong style="color: white; font-size: 150%"><em>or</em></strong>
+				<br />
+				<p>
+					Copy the address below and paste it into your wallet's baking feature.
+				</p>
+				<div
+					id="delegate-address-container"
+					@click="copyToClipboard(baktzDelegateAddress)"
+				>
+					<div id="delegate-address">
+						{{ baktzDelegateAddress }}
+					</div>
+					<nuxt-img
+						alt="Clipboard"
+						src="clipboard.svg"
+						style="aspect-ratio: 1/1"
+					/>
+				</div>
+			</span>
+		</client-only>
 	</div>
 </template>
 <script setup lang="ts">
@@ -52,11 +64,7 @@ import { notify } from '@kyvg/vue3-notification'
 import { useNuxtApp } from 'nuxt/app'
 import { baktzDelegateAddress } from '~/constants'
 
-const { $delegate, $activeAccount } = useNuxtApp()
-
-function switchWallet() {
-	console.log('not implemented yet')
-}
+const { $delegate, $activeAccount, $disconnect } = useNuxtApp()
 
 async function copyToClipboard(text: string): Promise<void> {
 	try {
--- plugins/wallet.client.ts
+++ plugins/wallet.client.ts
@@ -1,5 +1,19 @@
 import { defineNuxtPlugin } from 'nuxt/app'
 import { baktzDelegateAddress } from '~/constants'
+import { ref } from 'vue'
+
+type Delegate = {
+	alias: string
+	address: string
+	active: boolean
+}
+
+async function getDelegate(address: string): Promise<Delegate | null> {
+	const request = `https://api.tzkt.io/v1/accounts?address=${address}`
+	const response = await fetch(request)
+	const [{ delegate }] = await response.json()
+	return delegate
+}
 
 export default defineNuxtPlugin(async () => {
 	const { RpcClient } = await import('@taquito/rpc')
@@ -11,10 +25,12 @@ export default defineNuxtPlugin(async () => {
 
 	const { DAppClient, TezosOperationType } = await import('@airgap/beacon-dapp')
 	const dAppClient = new DAppClient({ name: `baktz` })
-	const activeAccount = await dAppClient.getActiveAccount()
+	const activeAccount = ref(await dAppClient.getActiveAccount())
 
-	if (activeAccount?.address) {
-		const delegateAddress = await tezosNode.getDelegate(activeAccount?.address)
+	if (activeAccount?.value?.address) {
+		const delegateAddress = await tezosNode.getDelegate(
+			activeAccount?.value?.address
+		)
 		if (delegateAddress === baktzDelegateAddress) {
 			// do something
 		}
@@ -33,18 +49,27 @@ export default defineNuxtPlugin(async () => {
 					}
 
 					if (account) {
-						await dAppClient.requestOperation({
-							operationDetails: [
-								{
-									kind: TezosOperationType.DELEGATION,
-									delegate: baktzDelegateAddress,
-								},
-							],
-						})
+						const delegate = await getDelegate(account.address)
+						if (delegate == null || delegate.address !== baktzDelegateAddress) {
+							await dAppClient.requestOperation({
+								operationDetails: [
+									{
+										kind: TezosOperationType.DELEGATION,
+										delegate: baktzDelegateAddress,
+									},
+								],
+							})
+						}
+						activeAccount.value = account
+
 					}
 				} catch (error) {
 					console.log(error)
 				}
+			},
+			async disconnect() {
+				await dAppClient.clearActiveAccount()
+				activeAccount.value = await dAppClient.getActiveAccount()
 			},
 		},
 	}
